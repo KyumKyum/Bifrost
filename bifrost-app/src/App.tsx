2fd53b06831293d7ec6ca800c5a94435c3884331
KyumKyum <mycheesepasta@gmail.com>
2024-08-13 13:04:28 +0900

    init: app

---
+++ bifrost-app/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   setGreetMsg(await invoke("greet", { name }));
+  // }
+
+  return (
+    <div className="h-svh flex flex-col bg-slate-700 p-10">
+      <h1 className="font-Yeongdo text-white text-8xl font-boldpb-0">BIFROST</h1>
+      <p className="font-PyeongChang text-white">Multi-purpose Utility & Storage Desktop Application</p>
+    </div>
+  );
+}
+
+export default App;
